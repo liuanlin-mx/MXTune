f5534b40f06ad6db7f2fb943a0a399cb99a89886
liuanlin-mx <liuanlin-mx@qq.com>
2022-04-23 00:51:40 +0800

    fix: rb scale err

--- pitch_shifter_rb.cpp
+++ pitch_shifter_rb.cpp
@@ -10,11 +10,14 @@ pitch_shifter_rb::pitch_shifter_rb(std::uint32_t sample_rate)
     , _sample_rate(sample_rate)
     , _rb(sample_rate, 1,
         RubberBand::RubberBandStretcher::OptionProcessRealTime
-        | RubberBand::RubberBandStretcher::OptionTransientsSmooth
+        //| RubberBand::RubberBandStretcher::OptionTransientsSmooth
+        //| RubberBand::RubberBandStretcher::OptionPitchHighConsistency
+        | RubberBand::RubberBandStretcher::OptionWindowStandard
+        | RubberBand::RubberBandStretcher::OptionDetectorSoft
+        | RubberBand::RubberBandStretcher::OptionThreadingNever
+        | RubberBand::RubberBandStretcher::OptionFormantPreserved
         | RubberBand::RubberBandStretcher::OptionPitchHighConsistency
-        | RubberBand::RubberBandStretcher::OptionWindowShort
-        | RubberBand::RubberBandStretcher::OptionDetectorPercussive
-        | RubberBand::RubberBandStretcher::OptionThreadingAuto)
+        )
 {
     _rb.setPitchScale(1.0);
 }
@@ -25,20 +28,24 @@ pitch_shifter_rb::~pitch_shifter_rb()
 
 void pitch_shifter_rb::update_shifter_variables(float inpitch, float outpitch)
 {
-    float scale;
-    if (inpitch > outpitch)
+    float scale = 1;
+    
+    float pitch_diff = (outpitch - inpitch);
+    if (pitch_diff > -12 && pitch_diff < 12)
     {
-        scale = 1.0 - (inpitch - outpitch) / 12.;
-    }
-    else
-    {
-        scale = 1.0 + (outpitch - inpitch) / 12.;
+        scale = pow(2., pitch_diff / 12.);
     }
     
-    if (scale < 3.0 && scale > 0.33)
+    if (scale < 0.5)
     {
-        _rb.setPitchScale(scale);
+        scale = 0.5;
     }
+    if (scale > 2)
+    {
+        scale = 2;
+    }
+    
+    _rb.setPitchScale(scale);
 }
 
 
